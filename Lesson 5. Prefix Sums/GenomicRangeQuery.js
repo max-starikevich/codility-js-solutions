6ababbe1620c5f76f8c115caacc696b1a1b77529
Max Starikevich <maxim.starikevich@gmail.com>
2018-12-12 16:14:06 +0300

    More robust version of GenomicRangeQuery solution

--- Lesson 5. Prefix Sums/GenomicRangeQuery.js
+++ Lesson 5. Prefix Sums/GenomicRangeQuery.js
@@ -1,6 +1,4 @@
 function solution (S, P, Q) {
-  let impacts = []
-
   const dnaImpactDictionary = {
     A: 1,
     C: 2,
@@ -8,18 +6,24 @@ function solution (S, P, Q) {
     T: 4
   }
 
-  const dnaLetters = Object.keys(dnaImpactDictionary)
+  const dnaLettersSorted = []
 
-  for (let i = 0; i < P.length; i++) {
-    let sequence = S.slice(P[i], Q[i] + 1)
+  Object.entries(dnaImpactDictionary).forEach(([letter, impact]) => {
+    dnaLettersSorted[impact] = letter
+  })
+
+  dnaLettersSorted.sort()
+
+  return P.map((start, index) => {
+    let end = Q[index]
+    let sequence = S.slice(start, end + 1)
+
     let letterIndex = 0
 
-    while (sequence.indexOf(dnaLetters[letterIndex]) === -1) {
+    while (sequence.indexOf(dnaLettersSorted[letterIndex]) === -1) {
       letterIndex++
     }
 
-    impacts.push(dnaImpactDictionary[dnaLetters[letterIndex]])
-  }
-
-  return impacts
+    return dnaImpactDictionary[dnaLettersSorted[letterIndex]]
+  })
 }
